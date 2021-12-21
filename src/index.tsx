ae9e1ccb4a2a63e2fb672d011c2cc2749b66e0a0
Antony Bartlett <antony.bartlett@ukri.org>
2021-12-21 09:46:49 +0000

    Hello world

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FunctionComponent = () => {
+    return <div>Hello world</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
