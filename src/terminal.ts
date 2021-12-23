9e91f5832429776f897a856e195d8a4b80fe6148
Antony Bartlett <antony.bartlett@ukri.org>
2021-12-23 15:04:50 +0000

    Deal with status codes when fetching wapm packages.

--- src/terminal.ts
+++ src/terminal.ts
@@ -7,10 +7,17 @@ export const createTerminal = () => {
             const url = `./wapm_packages/${program}/${program}.wasm`;
 
             const response = await fetch(url);
-            const arrayBuffer = await response.arrayBuffer();
-            return new Uint8Array(arrayBuffer);
+            switch (response.status) {
+            case 200:
+                const arrayBuffer = await response.arrayBuffer();
+                return new Uint8Array(arrayBuffer);
+            case 404:
+                throw new Error(`Program '${program}' is not part of this project`);
+            default:
+                throw new Error(`Failed to fetch '${program}', ${response.status} ${response.statusText}`)
+            }
         },
-        processWorkerUrl: './node_modules/@wasmer/wasm-terminal/lib/workers/process.worker.js'
+        processWorkerUrl: './node_modules/@wasmer/wasm-terminal/lib/workers/process.worker.js',
     });
 
     return wasmTerminal;
